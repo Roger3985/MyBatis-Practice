eb945e39f464daaa33abdfb87d95fabf6a9721e7
zhengchangsun <a0967067696@gmail.com>
2024-07-22 10:46:23 +0800

    new

---
+++ mybatis-004-web/src/main/java/com/powernode/WebSpringApplication.java
@@ -0,0 +1,14 @@
+package com.powernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Roger
+ */
+@SpringBootApplication
+public class WebSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringApplication.class, args);
+    }
+}
