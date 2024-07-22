8c281ce88fca3f9d07574971ecfbfbc3a892047c
zhengchangsun <a0967067696@gmail.com>
2024-07-22 09:34:20 +0800

    write the mybatis source pass

---
+++ godbatis/src/main/java/org/god/ibatis/core/SqlSessionFactoryBuilder.java
@@ -0,0 +1,12 @@
+package org.god.ibatis.core;
+
+import java.io.InputStream;
+
+/**
+ * SqlSessionFactory 建構器物件
+ * 通過 SqlSessionFactoryBuilder 的 build 方法解析
+ * godbatis-config.xml 文件，然後創建 SqlSessionFactory 物件
+ */
+public class SqlSessionFactoryBuilder {
+    public SqlSessionFactory build(InputStream )
+}
---
+++ godbatis/src/main/java/org/god/ibatis/utils/Resources.java
@@ -0,0 +1,25 @@
+package org.god.ibatis.utils;
+
+import java.io.InputStream;
+
+/**
+ * godbatis 框架提供的一個工具類
+ * 這個工具類專門完成"類路徑"中資源的加載
+ * @author Roger
+ * @version 1.0
+ * @since 1.0
+ */
+public class Resources {
+
+    /**
+     * 工具類的建構方法都是建設私有化的。
+     * 因為工具類中的方法都是靜態的，不需要創建物件就能調用。
+     * 為了避免 new 物件，所有的建構方法私有化。
+     * 這只是一種編程習慣。
+     */
+    private Resources() {}
+
+    public static InputStream getResourceAsStream(String resource) {
+        return ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
+    }
+}
