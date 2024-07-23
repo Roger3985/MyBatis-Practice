54f6f38aaba3861b80d9d05ec57f413a61f9c43e
a39849686 <a0967067696@gmail.com>
2024-07-23 08:21:25 +0800

    wrong on one data

--- mybatis-008-select/src/main/java/com/powernode/mybatis/mapper/CarMapper.java
+++ mybatis-008-select/src/main/java/com/powernode/mybatis/mapper/CarMapper.java
@@ -2,7 +2,23 @@ package com.powernode.mybatis.mapper;
 
 import com.powernode.mybatis.pojo.Car;
 
+import java.util.List;
+
 public interface CarMapper {
+
+    /**
+     * 根據品牌進行模糊查詢。
+     * 模糊查詢的結果可能會有多個，但是我採用一個 POJO 物件來接收會有問題嗎？
+     * @param brand 品牌
+     * @return Car
+     */
+    Car selectByBrandLike(String brand);
+
+    /**
+     * 獲取所有的 Car
+     * @return List
+     */
+    List<Car> selectAll();
 
     /**
      * 根據 id 查詢 Car 信息
--- mybatis-008-select/src/test/java/com/powernode/mybatis/mapper/CarMapperTest.java
+++ mybatis-008-select/src/test/java/com/powernode/mybatis/mapper/CarMapperTest.java
@@ -5,7 +5,29 @@ import com.powernode.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 public class CarMapperTest {
+
+    @Test
+    public void testSelectByBrandLike() {
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        CarMapper mapper = sqlSession.getMapper(CarMapper.class);
+        // ToManyResultsException
+        // 什麼意思？你期望的結果是返回一條資料，但是實際上的 SQlL 語句在執行時，，返回的紀錄條數不是一條
+        Car car = mapper.selectByBrandLike("比亞迪");
+        System.out.println(car);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        CarMapper mapper = sqlSession.getMapper(CarMapper.class);
+        List<Car> cars = mapper.selectAll();
+        cars.forEach(car -> System.out.println(car));
+        sqlSession.close();
+    }
 
     @Test
     public void testSelectById() {
