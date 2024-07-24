694b660be85a126d6f4e0ba65279352051eb7158
a39849686 <a0967067696@gmail.com>
2024-07-24 23:27:47 +0800

    godbatis framework test

--- godbatis-test/src/test/java/com/powernode/godbatis/test/UserMapperTest.java
+++ godbatis-test/src/test/java/com/powernode/godbatis/test/UserMapperTest.java
@@ -1,20 +1,13 @@
-package com.god.ibatis.test;
+package com.powernode.godbatis.test;
 
-import com.god.ibatis.pojo.User;
+import com.powernode.godbatis.pojo.User;
 import org.god.ibatis.core.SqlSession;
 import org.god.ibatis.core.SqlSessionFactory;
 import org.god.ibatis.core.SqlSessionFactoryBuilder;
 import org.god.ibatis.utils.Resources;
 import org.junit.Test;
 
-public class GodBatisTest {
-
-    @Test
-    public void testSqlSessionFactory() {
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("godbatis-config.xml"));
-        System.out.println(sqlSessionFactory);
-    }
+public class UserMapperTest {
 
     @Test
     public void testInsertUser() {
